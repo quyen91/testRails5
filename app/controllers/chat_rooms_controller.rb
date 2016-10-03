1c363f54a23317fdd98cf8e0976a25f84b584c19
quyen <chibedethuong91@gmail.com>
2016-10-03 12:12:38 +0700

    add custom css

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -17,7 +17,7 @@ class ChatRoomsController < ApplicationController
   end
 
   def show
-    @chat_room = ChatRoom.includes(:message).find_by(id: params[:id])
+    @chat_room = ChatRoom.includes(:messages).find_by(id: params[:id])
     @message = Message.new
   end
 
